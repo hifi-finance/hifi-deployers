fe4a8c1837a49e9ef80273a7dec4e64beb4c8bfa
Paul Razvan Berg <hello@paulrberg.com>
2021-08-17 11:14:45 +0300

    refactor: optional 2nd pair address for HifiFlashUniswapV2 deployer

--- tasks/deploy/hifiFlashUniswapV2.ts
+++ tasks/deploy/hifiFlashUniswapV2.ts
@@ -8,12 +8,17 @@ import { TASK_DEPLOY_HIFI_FLASH_UNISWAP_V2 } from "../constants";
 
 task(TASK_DEPLOY_HIFI_FLASH_UNISWAP_V2)
   .addParam("balanceSheet", "The address of the BalanceSheet contract")
-  .addParam("uniswapV2Pair", "The address of the Uniswap V2 pair contract")
+  .addParam("pair0", "The address of an Uniswap V2 pair contract")
+  .addOptionalParam("pair1", "The address of an Uniswap V2 pair contract")
   .setAction(async function (taskArgs: TaskArguments, { ethers }): Promise<string> {
     const signers: SignerWithAddress[] = await ethers.getSigners();
     const hifiFlashUniswapV2Factory: HifiFlashUniswapV2__factory = new HifiFlashUniswapV2__factory(signers[0]);
+    const pairs: string[] = [taskArgs.pair0];
+    if (taskArgs.pair1) {
+      pairs.push(taskArgs.pair1);
+    }
     const hifiFlashUniswapV2: HifiFlashUniswapV2 = <HifiFlashUniswapV2>(
-      await hifiFlashUniswapV2Factory.deploy(taskArgs.balanceSheet, [taskArgs.uniswapV2Pair])
+      await hifiFlashUniswapV2Factory.deploy(taskArgs.balanceSheet, pairs)
     );
     await hifiFlashUniswapV2.deployed();
     return hifiFlashUniswapV2.address;
